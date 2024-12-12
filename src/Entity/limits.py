cd74980abf1bbd41caf98db2b17c83c7993b0f61
v-k-1-8 <lakhiyanivarun14@gmail.com>
2024-12-12 23:58:19 +0530

    Server created with endpoint for parameters extractions

---
+++ src/Entity/limits.py
@@ -0,0 +1,312 @@
+limits = {
+    "Hemoglobin": {
+        "unit": "g/dL",
+        "M": {"lower": 14, "upper": 17},
+        "F": {"lower": 12, "upper": 15},
+    },
+    "WBC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 4500, "upper": 10000},
+        "F": {"lower": 4500, "upper": 10000},
+    },
+    "RBC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 4.7, "upper": 6.1},
+        "F": {"lower": 4.2, "upper": 5.4},
+    },
+    "MCV": {
+        "unit": "femtoliters",
+        "M": {"lower": 80, "upper": 100},
+        "F": {"lower": 80, "upper": 100},
+    },
+    "MCH": {
+        "unit": "picograms",
+        "M": {"lower": 26, "upper": 34},
+        "F": {"lower": 26, "upper": 34},
+    },
+    "MCHC": {
+        "unit": "g/dL",
+        "M": {"lower": 32, "upper": 36},
+        "F": {"lower": 32, "upper": 36},
+    },
+    "Hematocrit": {
+        "unit": "%",
+        "M": {"lower": 41, "upper": 50},
+        "F": {"lower": 36, "upper": 44},
+    },
+    "RDW": {
+        "unit": "%",
+        "M": {"lower": 11.5, "upper": 14.5},
+        "F": {"lower": 11.5, "upper": 14.5},
+    },
+    "Platelet Count": {
+        "unit": "cells/mcL",
+        "M": {"lower": 140000, "upper": 450000},
+        "F": {"lower": 140000, "upper": 450000},
+    },
+    "MPV": {
+        "unit": "femtoliters",
+        "M": {"lower": 7.5, "upper": 11.5},
+        "F": {"lower": 7.5, "upper": 11.5},
+    },
+    "Neutrophils": {
+        "unit": "%",
+        "M": {"lower": 40, "upper": 80},
+        "F": {"lower": 40, "upper": 80},
+    },
+    "Lymphocytes": {
+        "unit": "%",
+        "M": {"lower": 20, "upper": 40},
+        "F": {"lower": 20, "upper": 40},
+    },
+    "Monocytes": {
+        "unit": "%",
+        "M": {"lower": 2, "upper": 10},
+        "F": {"lower": 2, "upper": 10},
+    },
+    "Eosinophils": {
+        "unit": "%",
+        "M": {"lower": 1, "upper": 6},
+        "F": {"lower": 1, "upper": 6},
+    },
+    "Basophils": {
+        "unit": "%",
+        "M": {"lower": 0, "upper": 2},
+        "F": {"lower": 0, "upper": 2},
+    },
+    "ESR": {
+        "unit": "mm/hr",
+        "M": {"lower": 0, "upper": 15},
+        "F": {"lower": 0, "upper": 20},
+    },
+    "ANC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 2000, "upper": 7000},
+        "F": {"lower": 2000, "upper": 7000},
+    },
+    "ALC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 1000, "upper": 4800},
+        "F": {"lower": 1000, "upper": 4800},
+    },
+    "AMC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 200, "upper": 800},
+        "F": {"lower": 200, "upper": 800},
+    },
+    "AEC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 0, "upper": 500},
+        "F": {"lower": 0, "upper": 500},
+    },
+    "ABC": {
+        "unit": "cells/mcL",
+        "M": {"lower": 0, "upper": 100},
+        "F": {"lower": 0, "upper": 100},
+    },
+    "NLR": {
+        "unit": "ratio",
+        "M": {"lower": 1, "upper": 3},
+        "F": {"lower": 1, "upper": 3},
+    },
+    "HbA1C": {
+        "unit": "%",
+        "M": {"lower": 0, "upper": 5.7},
+        "F": {"lower": 0, "upper": 5.7},
+    },
+    "Mean Blood Glucose": {
+        "unit": "mg/dL",
+        "M": {"lower": 70, "upper": 100},
+        "F": {"lower": 70, "upper": 100},
+    },
+    "Triiodothyronine": {
+        "unit": "ng/mL",
+        "M": {"lower": 0.8, "upper": 2.0},
+        "F": {"lower": 0.8, "upper": 2.0},
+    },
+    "Thyroxine": {
+        "unit": "µg/dL",
+        "M": {"lower": 5.0, "upper": 12.0},
+        "F": {"lower": 5.0, "upper": 12.0},
+    },
+    "Thyroid Stimulating Hormone": {
+        "unit": "µIU/mL",
+        "M": {"lower": 0.4, "upper": 4.0},
+        "F": {"lower": 0.4, "upper": 4.0},
+    },
+    "Cholesterol": {
+        "unit": "mg/dL",
+        "M": {"lower": 0, "upper": 200},
+        "F": {"lower": 0, "upper": 200},
+    },
+    "Triglycerides": {
+        "unit": "mg/dL",
+        "M": {"lower": 0, "upper": 150},
+        "F": {"lower": 0, "upper": 150},
+    },
+    "HDL": {
+        "unit": "mg/dL",
+        "M": {"lower": 50, "upper": 60},
+        "F": {"lower": 50, "upper": 60},
+    },
+    "Non-HDL": {
+        "unit": "mg/dL",
+        "M": {"lower": 0, "upper": 130},
+        "F": {"lower": 0, "upper": 130},
+    },
+    "LDL": {
+        "unit": "mg/dL",
+        "M": {"lower": 0, "upper": 100},
+        "F": {"lower": 0, "upper": 100},
+    },
+    "VLDL": {
+        "unit": "mg/dL",
+        "M": {"lower": 2, "upper": 30},
+        "F": {"lower": 2, "upper": 30},
+    },
+    "LDL/HDL Ratio": {
+        "unit": "ratio",
+        "M": {"lower": 0, "upper": 3},
+        "F": {"lower": 0, "upper": 3},
+    },
+    "HDL/LDL Ratio": {
+        "unit": "ratio",
+        "M": {"lower": 0.4, "upper": 0.6},
+        "F": {"lower": 0.6, "upper": 0.6},
+    },
+    "CHOL/HDL Ratio": {
+        "unit": "ratio",
+        "M": {"lower": 0, "upper": 4.5},
+        "F": {"lower": 0, "upper": 4.5},
+    },
+    "Creatinine": {
+        "unit": "mg/dL",
+        "M": {"lower": 0.74, "upper": 1.35},
+        "F": {"lower": 0.59, "upper": 1.04},
+    },
+    "BUN/Creatinine Ratio": {
+        "unit": "ratio",
+        "M": {"lower": 10, "upper": 20},
+        "F": {"lower": 10, "upper": 20},
+    },
+    "Urea": {
+        "unit": "mg/dL",
+        "M": {"lower": 7, "upper": 20},
+        "F": {"lower": 7, "upper": 20},
+    },
+    "Uric Acid": {
+        "unit": "mg/dL",
+        "M": {"lower": 3.4, "upper": 7.0},
+        "F": {"lower": 2.4, "upper": 6.0},
+    },
+    "Blood Urea Nitrogen": {
+        "unit": "mg/dL",
+        "M": {"lower": 6, "upper": 20},
+        "F": {"lower": 6, "upper": 20},
+    },
+    "Calcium": {
+        "unit": "mg/dL",
+        "M": {"lower": 8.5, "upper": 10.2},
+        "F": {"lower": 8.5, "upper": 10.2},
+    },
+    "Phosphorus": {
+        "unit": "mg/dL",
+        "M": {"lower": 2.5, "upper": 4.5},
+        "F": {"lower": 2.5, "upper": 4.5},
+    },
+    "SGPT": {
+        "unit": "U/L",
+        "M": {"lower": 0, "upper": 40},
+        "F": {"lower": 0, "upper": 40},
+    },
+    "SGOT": {
+        "unit": "U/L",
+        "M": {"lower": 0, "upper": 40},
+        "F": {"lower": 0, "upper": 40},
+    },
+    "eGFR": {
+        "unit": "mL/min/1.73m²",
+        "M": {"lower": 90, "upper": 120},
+        "F": {"lower": 90, "upper": 120},
+    },
+    "Sodium": {
+        "unit": "mmol/L",
+        "M": {"lower": 135, "upper": 145},
+        "F": {"lower": 135, "upper": 145},
+    },
+    "Potassium": {
+        "unit": "mmol/L",
+        "M": {"lower": 3.5, "upper": 5.0},
+        "F": {"lower": 3.5, "upper": 5.0},
+    },
+    "Chloride": {
+        "unit": "mmol/L",
+        "M": {"lower": 98, "upper": 107},
+        "F": {"lower": 98, "upper": 107},
+    },
+    "Magnesium": {
+        "unit": "mg/dL",
+        "M": {"lower": 1.6, "upper": 2.3},
+        "F": {"lower": 1.6, "upper": 2.3},
+    },
+    "Total Bilirubin": {
+        "unit": "mg/dL",
+        "M": {"lower": 0.2, "upper": 1.2},
+        "F": {"lower": 0.2, "upper": 1.2},
+    },
+    "Conjugated Bilirubin": {
+        "unit": "mg/dL",
+        "M": {"lower": 0, "upper": 0.3},
+        "F": {"lower": 0, "upper": 0.3},
+    },
+    "Unconjugated Bilirubin": {
+        "unit": "mg/dL",
+        "M": {"lower": 0.1, "upper": 1.0},
+        "F": {"lower": 0.1, "upper": 1.0},
+    },
+    "AST": {
+        "unit": "U/L",
+        "M": {"lower": 10, "upper": 40},
+        "F": {"lower": 10, "upper": 40},
+    },
+    "ALT": {
+        "unit": "U/L",
+        "M": {"lower": 7, "upper": 56},
+        "F": {"lower": 7, "upper": 56},
+    },
+    "AST/ALT Ratio": {
+        "unit": "ratio",
+        "M": {"lower": 0.7, "upper": 1.4},
+        "F": {"lower": 0.7, "upper": 1.4},
+    },
+    "ALP": {
+        "unit": "U/L",
+        "M": {"lower": 44, "upper": 147},
+        "F": {"lower": 44, "upper": 147},
+    },
+    "GGT": {
+        "unit": "U/L",
+        "M": {"lower": 9, "upper": 48},
+        "F": {"lower": 9, "upper": 48},
+    },
+    "Protein": {
+        "unit": "g/dL",
+        "M": {"lower": 6.4, "upper": 8.3},
+        "F": {"lower": 6.4, "upper": 8.3},
+    },
+    "Albumin": {
+        "unit": "g/dL",
+        "M": {"lower": 3.5, "upper": 5.0},
+        "F": {"lower": 3.5, "upper": 5.0},
+    },
+    "Globulin": {
+        "unit": "g/dL",
+        "M": {"lower": 2.3, "upper": 3.5},
+        "F": {"lower": 2.3, "upper": 3.5},
+    },
+    "A/G Ratio": {
+        "unit": "ratio",
+        "M": {"lower": 1.0, "upper": 2.1},
+        "F": {"lower": 1.0, "upper": 2.1},
+    }
+}
